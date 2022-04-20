97e2f579b933d6f2b50415eb786dbb6396186fce
Sung Yoo <yoowook1207@gmail.com>
2022-04-20 15:42:16 -0400

    stripe api and lazyquery

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,8 +5,14 @@ import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
     const [state, dispatch] = useStoreContext();
 
     useEffect(() => {
@@ -19,6 +25,14 @@ const Cart = () => {
           getCart();
         }
       }, [state.cart.length, dispatch]);
+
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+            });
+        }
+    }, [data]);
 
     function toggleCart() {
     dispatch({ type: TOGGLE_CART });
@@ -44,6 +58,20 @@ const Cart = () => {
 
     console.log(state)
 
+    function submitCheckout() {
+        const productIds = [];
+      
+        state.cart.forEach((item) => {
+          for (let i = 0; i < item.purchaseQuantity; i++) {
+            productIds.push(item._id);
+          }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+          });
+      }
+
     return (
         <div className="cart">
             <div className="close" onClick={toggleCart}>[close]</div>
@@ -57,7 +85,7 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                         Auth.loggedIn() ?
-                            <button>
+                            <button onClick={submitCheckout}>
                             Checkout
                             </button>
                             :
